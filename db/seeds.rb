75d91047b1d63894a31dfb2a103d02c51fb13c78
Jess <jessica.stevens2115@gmail.com>
2022-03-17 16:14:49 -0600

    Added DB and some more styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+Song.create([{title: 'Thunderous', artist: 'Stray Kids', album: 'https://upload.wikimedia.org/wikipedia/en/5/59/Stray_Kids_-_Noeasy_%28Digital%29.jpeg'}, {title: 'Mikrokosmos', artist: 'BTS', album: 'https://m.media-amazon.com/images/I/51wUYZ1HYmL._SL1200_.jpg'}, {title: 'Hello Future', artist: 'NCT DREAM', album: 'https://i.scdn.co/image/ab67616d0000b273e6d118f2ad157bf0bbfb83cf'}, {title: 'Lion', artist: 'Saint Mesa', album: 'https://m.media-amazon.com/images/I/81gn0vk-YKL._SS500_.jpg'}, {title: 'Wellerman', artist: 'Nathan Evans', album: 'https://i1.sndcdn.com/artworks-9mbzFxHNT4IEsl3D-aEyVbA-t500x500.jpg'}])
